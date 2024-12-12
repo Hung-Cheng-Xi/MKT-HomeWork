b939a2f495b16ddf2f0683d6c9a29c1829da5ad0
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-12-12 14:46:04 +0800

    (feat): 替換 model

--- service/check_tag.py
+++ service/check_tag.py
@@ -1,9 +1,11 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import AutoModelForSequenceClassification, BertTokenizer
 
 # 加載模型和標籤映射
-model_name = "juliensimon/reviews-sentiment-analysis"
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+tokenizer = BertTokenizer.from_pretrained(model_name)
 
-# 查看標籤映射
-print(model.config.id2label)
+if __name__ == '__main__':
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=5)
+
+    # 查看標籤映射
+    print(model.config.id2label)
