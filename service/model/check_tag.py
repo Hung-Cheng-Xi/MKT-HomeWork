85f4c9324947b30371bbb89d9b44c7c8d251c618
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-12-23 02:27:03 +0800

    (refactor): format

--- service/model/check_tag.py
+++ service/model/check_tag.py
@@ -1,13 +1,13 @@
 from transformers import AutoModelForSequenceClassification, BertTokenizer
 
 # 加載模型和標籤映射
-model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+model_name = 'nlptown/bert-base-multilingual-uncased-sentiment'
 tokenizer = BertTokenizer.from_pretrained(model_name)
 
-if __name__ == "__main__":
-    model = AutoModelForSequenceClassification.from_pretrained(
-        model_name, num_labels=5
-    )
+if __name__ == '__main__':
+	model = AutoModelForSequenceClassification.from_pretrained(
+		model_name, num_labels=5
+	)
 
-    # 查看標籤映射
-    print(model.config.id2label)
+	# 查看標籤映射
+	print(model.config.id2label)
