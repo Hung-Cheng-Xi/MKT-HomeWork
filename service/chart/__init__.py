9f622c7741bfc6450b7782cb07903b9df2838cc3
Yicheng0093 <yicheng814071@gmail.com>
2025-01-02 02:30:03 +0800

    食物排名前10筆(圓餅圖)

--- service/chart/__init__.py
+++ service/chart/__init__.py
@@ -1,3 +1,4 @@
 from .chart_assuracy import plot_model_accuracy
+from .chart_top_ten_food import plot_top_foods_pie_chart
 
-__all__ = ["plot_model_accuracy"]
+__all__ = ["plot_model_accuracy", plot_top_foods_pie_chart]
